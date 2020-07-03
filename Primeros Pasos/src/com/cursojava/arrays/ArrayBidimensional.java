cc79995f6e8c79a4bf8677d4fb90810a9e3dd4ca
Cobo Morera <cobo.morera.dev@gmail.com>
2020-07-03 17:01:57 +0100

    Inicio sección Programación Orientada a Objetos

--- Primeros Pasos/src/com/cursojava/arrays/ArrayBidimensional.java
+++ Primeros Pasos/src/com/cursojava/arrays/ArrayBidimensional.java
@@ -4,21 +4,43 @@ import java.util.Random;
 public class ArrayBidimensional {
 
 	public static void main(String[] args) {
+//		int[][] numeros = new int[5][3];
+//		
+//		for( int i = 0; i < 5; i ++) {
+//			for (int j = 0; j < 3; j ++){
+//				Random r = new Random();
+//				numeros[i][j] = r.nextInt((100 - 1) + 1 ) + 1;
+//			}
+//		}
+//		
+//		
+		
 		int[][] numeros = new int[5][3];
 		
 		for( int i = 0; i < 5; i ++) {
 			for (int j = 0; j < 3; j ++){
-				Random r = new Random();
-				numeros[i][j] = r.nextInt((100 - 1) + 1 ) + 1;
+				
+				numeros[i][j] = (int) (Math.random() * 100 )+ 1;
 			}
 		}
 		
 		
-		for( int i = 0; i < 5; i ++) {
-			for (int j = 0; j < 3; j ++){
-				System.out.println(numeros[i][j] );
-				
+		
+		
+//		for( int i = 0; i < 5; i ++) {
+//			for (int j = 0; j < 3; j ++){
+//				System.out.print(numeros[i][j]+ " " );
+//				
+//			}
+//		}
+		
+//		System.out.println();
+		
+		for(int[] dimension1 : numeros) {
+			for(int dimension2: dimension1) {
+				System.out.print(dimension2 + "\t ");
 			}
+			System.out.println();
 		}
 		
 		
