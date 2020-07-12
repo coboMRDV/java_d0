71d3f4e740a8256e8e7a955f6693da8683bed972
Cobo Morera <cobo.morera.dev@gmail.com>
2020-07-12 13:49:33 +0100

    Terminado ejercicio barra de menús

---
+++ Curso de Java/src/com/cursojava/GUIs/componentesSwing/menu/UsoMenu.java
@@ -0,0 +1,97 @@
+package com.cursojava.GUIs.componentesSwing.menu;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+public class UsoMenu {
+
+	public static void main(String[] args) {
+		Frame frame = new Frame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
+
+class Frame extends JFrame {
+
+	public Frame() {
+		this.setSize(400, 300);
+		this.setLocationRelativeTo(null);
+
+		this.add(new Panel());
+	}
+
+}
+
+class Panel extends JPanel  {
+	JMenuBar menuBar;
+
+	public Panel() {
+		this.setBackground(new Color(55, 55, 55));
+		this.setLayout(new BorderLayout());
+		
+		
+//		===========  MENU ============	//
+		menuBar = new JMenuBar();
+		/*
+		 * menu archivo
+		 */
+		JMenu menuArchivo = new JMenu("Archivo");
+		JMenuItem itemGuardar = new JMenuItem("Guardar");
+		JMenuItem itemGuardarComo = new JMenuItem("Guardar como...");
+		menuArchivo.add(itemGuardar);
+		menuArchivo.add(itemGuardarComo);
+//		**acciones**
+		itemGuardar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Guardar");
+			}
+		});
+
+		/*
+		 * menu edición
+		 */
+		JMenu menuEdicion = new JMenu("Edición");
+		JMenuItem itemCortar = new JMenuItem("Cortar");
+		JMenuItem itemCopiar = new JMenuItem("Copiar");
+		JMenuItem itemPegar = new JMenuItem("Pegar");
+		menuEdicion.add(itemCortar);
+		menuEdicion.add(itemCopiar);
+		menuEdicion.add(itemPegar);
+
+		/*
+		 * menu herramientas
+		 */
+		JMenu menuHerramientas = new JMenu("Herramientas");
+		JMenuItem itemOpciones = new JMenuItem("Opciones");
+		JMenu submenuPreferencias = new JMenu("Preferencias");
+		menuHerramientas.add(itemOpciones);
+		menuHerramientas.addSeparator();
+		menuHerramientas.add(submenuPreferencias);
+
+		JMenuItem itemGenerales = new JMenuItem("Generales");
+		JMenuItem itemAyuda = new JMenuItem("Ayuda");
+		submenuPreferencias.add(itemGenerales);
+		submenuPreferencias.add(itemAyuda);
+//		=========== FINAL DEL MENU ============	//
+
+		menuBar.add(menuArchivo);
+		menuBar.add(menuEdicion);
+		menuBar.add(menuHerramientas);
+
+		this.add(menuBar, BorderLayout.NORTH);
+
+	}
+}
